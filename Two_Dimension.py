c4e2901c20bff107c98ee54f711af63fea1b259a
Devika Shetty <124135758+shettydevika@users.noreply.github.com>
2024-02-26 11:13:42 +0530

    Create Two_Dimension.py

---
+++ Two_Dimension.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import random
+import math
+from collections import Counter
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+    low_z, low_p = -10.0, 0
+    hi_z, hi_p = 10.0, 1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2
+        mid_p = normal_cdf(mid_z)
+        if mid_p < p:
+            low_z, low_p = mid_z, mid_p
+        elif mid_p > p:
+            hi_z, hi_p = mid_z, mid_p
+        else:
+            break
+    return mid_z
+
+def normal_cdf(x, mu=0, sigma=1):
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+def random_normal():
+    return inverse_normal_cdf(random.random())
+
+xs = [random_normal() for _ in range(1000)]
+ys1 = [x + random_normal() / 2 for x in xs]
+ys2 = [-x + random_normal() / 2 for x in xs]
+
+plt.scatter(xs, ys1, marker='.', color='black', label='ys1')
+plt.scatter(xs, ys2, marker='.', color='gray', label='ys2')
+plt.xlabel('xs')
+plt.ylabel('ys')
+plt.legend(loc=9) 
+plt.title("Very Different Joint Distributions")
+plt.show()
